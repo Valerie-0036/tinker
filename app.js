d294c69c04860ef0d31f8eec5922eeff73658457
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2022-10-10 00:40:54 +0700

    asdasdsad

--- app.js
+++ app.js
@@ -1,8 +1,81 @@
-$('#comment-button').click((event)=>{
-  $('.comment').toggle();
+function comment(count) {
+  var bubbles = document.getElementsByClassName("interactive-bubble")[count];
+  var music = document.getElementsByClassName("music")[count];
+  var userCaption = document.getElementsByClassName("user-caption")[count];
 
-//  else {
-//     $('.others').attr('margin', '0');
-//  }
+  userCaption.classList.toggle("hidden");
+  music.classList.toggle("hidden");
+  bubbles.classList.toggle("small");
 
+  var comment = document.getElementsByClassName("comment-container")[count];
+  var captionComment = document.getElementsByClassName("caption-comment")[count];
+  var commentAdd = document.getElementsByClassName("comment-add")[count];
+
+  comment.classList.toggle("hidden");
+  commentAdd.classList.toggle("hidden");
+  captionComment.classList.toggle("show-comments");
+}
+
+function share(count){
+  var share = $('.share-container')[count];
+share.classList.toggle("hidden");
+};
+
+$('.like').click(function(event){
+  $(this).find('i').toggleClass("fa-solid");
 });
+
+$('.user-follow').click(function(event) {
+  if($(this).hasClass("follow")){
+    $(this).removeClass("follow").addClass("followed");
+    $(this).text("Followed");
+  } else {
+    $(this).removeClass("followed unfollow").addClass("follow");
+    $(this).text("Follow");
+  }
+});
+
+$('.user-follow').mouseenter(function(event) {
+  if($(this).hasClass("followed")){
+    $(this).text("Unfollow");
+    $(this).addClass("unfollow");
+  } 
+});
+
+
+$('.user-follow').mouseout(function(event) {
+  if($(this).hasClass("unfollow")){
+    $(this).text("Followed");
+    $(this).removeClass("unfollow");
+  }
+});
+
+/* PROFILE NAVBAR */
+$('.master,.profile-menu').mouseover(function(event){
+  $('.profile-menu').removeClass("hidden");
+  });
+  
+  $('.profile-menu').mouseout(function(event){
+    $('.profile-menu').addClass("hidden");
+    });
+  
+  $('main').mouseover(function(event){
+    $('.profile-menu').addClass("hidden");
+    });
+
+    $(document).mouseleave(function () {
+      $('.profile-menu').addClass("hidden");
+  });
+  //will be altered for clicking user's profile pic
+  // $('.master').click(function(event) {
+  //   var status = $(this).attr('id');
+  //  if(status===""){
+  
+  //  } else {
+  //   location.href=`profiles/${status}.html`;
+  //  }
+  // });
+
+    $('.master').click(function(event) {
+    location.href=`myprofile.html`;
+  });
